eb90799af2ba20b8c27e2da7ccac291e2d32cbd9
Shashank-Panda <shashankpanda54@gmail.com>
2021-05-27 19:06:08 +0530

    first few lines of JS

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,41 @@
-console.log("Hello World!");
+//elements
+const addBtn = document.querySelector('.todo-btn');
+const taskList = document.querySelector('.todo-list');
+const taskItem = document.querySelector('.todo-ip');
+const dltBtn = taskList.querySelector('.dlt-btn');
 
+//Event Listeners
+addBtn.addEventListener('click',addTask);
+//dltBtn.addEventListener('click',deleteTask);
+
+//Functions
+function addTask(event)
+{
+    event.preventDefault();
+    console.log(event.target);
+
+    const newTask = document.createElement('li');
+    newTask.classList.add('task-item');
+
+    const taskName = document.createElement('p');
+    taskName.innerText = taskItem.value;
+    taskName.classList.add('task-name');
+    newTask.appendChild(taskName);
+
+    const dlt = document.createElement('button');
+    dlt.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    dlt.classList.add('dlt-btn');
+    newTask.appendChild(dlt);
+
+    taskList.appendChild(newTask);
+
+    taskItem.value = "";
+}
+
+// function deleteTask(event)
+// {
+//     event.preventDefault();
+//     console.log(event.target);
+
+
+// }
